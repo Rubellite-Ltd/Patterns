76bb30eea1d5386a4bb6eb5f0cb6899baa83142d
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2024-03-13 13:13:20 +0000

    Update mrkscript.js

--- mrkscript.js
+++ mrkscript.js
@@ -1,1 +1,30 @@
 
+   <script>
+        // Function to toggle sidebar visibility
+        function toggleMenu() {
+            var sidebar = document.getElementById('sidebar');
+            sidebar.classList.toggle('active');
+        }
+
+        // Function to show individual content section
+        function showContent(contentId) {
+            var contents = document.querySelectorAll('.content');
+            contents.forEach(function(content) {
+                content.classList.remove('active');
+            });
+            document.getElementById(contentId).classList.add('active');
+
+            // Hide sidebar after clicking a button (only for small screens)
+            var sidebar = document.getElementById('sidebar');
+            if (window.innerWidth <= 768) {
+                sidebar.classList.remove('active');
+            }
+        }
+
+            document.addEventListener('DOMContentLoaded', function () {
+            var currentYear = new Date().getFullYear();
+            document.getElementById('copyright-year').innerText = '© ' + currentYear + ' Rubellite Ltd. All rights reserved.';
+        });
+
+        
+    </script>
