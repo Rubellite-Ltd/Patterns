60e306b290282861e971a0a346a189dd394dfce8
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2024-07-23 15:47:11 +0100

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,11 +1,19 @@
 const stripe = require('stripe')('sk_test_51INkUXIZflYtINDX2BJfxt3QoJ5hNV8WVZmjBAmHNHayZyPxtN9t6jVcTc774Z5mpOJVREnpddkaKSQEBrP6w14J00mxqZlddB');  // Replace with your test or live key
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method Not Allowed' }),
+    };
+  }
+
   try {
-    const { amount } = JSON.parse(event.body);
+    const { amount, currency } = JSON.parse(event.body);
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: 'usd',
+      currency,
     });
 
     return {
@@ -13,6 +21,7 @@ exports.handler = async (event) => {
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
     };
   } catch (error) {
+    console.error('Error creating payment intent:', error);
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
