ae109da6fd9ad4c58589370862130c9b8d17b945
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2024-07-23 10:28:26 +0100

    Create create-payment-intent.js

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')('sk_test_...');
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
