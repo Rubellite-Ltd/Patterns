5cebdab2afb1ba85377e7dcbe02e34f488f6b09c
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2024-07-23 16:10:23 +0100

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')('sk_test_51INkUXIZflYtINDX2BJfxt3QoJ5hNV8WVZmjBAmHNHayZyPxtN9t6jVcTc774Z5mpOJVREnpddkaKSQEBrP6w14J00mxqZlddB');  // Replace with your test or live key
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
